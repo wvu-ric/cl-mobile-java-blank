4ce0eed8c886201d58ccc85b72878c797ae18fb5
matthewbbrumley <matt@syntropy.io>
2016-02-02 13:48:11 -0500

    Initial Commit

---
+++ blank/BaseProgram.java
@@ -0,0 +1,14 @@
+public class BaseProgram {
+    
+    //Variable Declaration
+    
+    //Method Declaration
+    //main is the function that runs first and starts the lifecycle
+    public static void main(String[] args) {
+        
+        //Print out good old Hello World!
+        System.out.println("Hello World!");
+        
+    }
+    
+}
